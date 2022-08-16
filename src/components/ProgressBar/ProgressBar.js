110eefbef853e22db3e23b95a54c2743dec4b199
Max Sultan <msultan@aumni.fund>
2022-08-16 13:46:33 -0600

    Add Progress bar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,56 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const height = {
+  small: 8,
+  medium: 12,
+  large: 24,
+};
+
+const ProgressBarContainer = styled.div`
+  width: 370px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  height: ${({ size }) => `${height[size]}px;`};
+  overflow: hidden;
+`;
+
+const ProgressIndicatorBar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${({ value }) => `${value}%`};
+  height: 100%;
+`;
+
+const OuterWrapper = styled.div`
+  padding: 4px;
+  border-radius: 8px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  width: fit-content;
+
+  ${ProgressBarContainer} {
+    box-shadow: none;
+    background-color: transparent;
+    border-radius: 4px 4px 4px 4px;
+  }
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return size === "large" ? (
+    <OuterWrapper>
+      <ProgressBarContainer size={size}>
+        <ProgressIndicatorBar value={value} size={size}></ProgressIndicatorBar>
+      </ProgressBarContainer>
+    </OuterWrapper>
+  ) : (
+    <ProgressBarContainer size={size}>
+      <ProgressIndicatorBar value={value} size={size}></ProgressIndicatorBar>
+    </ProgressBarContainer>
+  );
 };
 
 export default ProgressBar;
