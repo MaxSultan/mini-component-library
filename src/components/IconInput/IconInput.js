45877ce82d4ba2910a341ad3093d0b335a8be7cc
Max Sultan <msultan@aumni.fund>
2022-08-23 12:47:21 -0600

    done

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const StyledInputWrapper = styled.div`
+    max-width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 30fr;
+    grid-template-rows: 100%;
+    border-bottom: 1px solid ${COLORS.black};
+
+    &:focus {
+      outline: 2px solid blue;
+    }
+  `;
+
+  const StyledLabel = styled.label`
+    width: ${({ width }) => width};
+  `;
+
+  const StyleResetInput = styled.input`
+    border: none;
+    outline: none;
+  `;
+
+  const triggerParentFocus = (e) => {
+    e.preventDefault();
+    e.target.parentElement.style = "outline: 2px solid blue";
+  };
+
+  const handleBlur = (e) => {
+    e.target.parentElement.style = "outline: none";
+  };
+
+  return (
+    <StyledLabel>
+      {label}
+      <StyledInputWrapper tabIndex="0" onBlur={(e) => handleBlur(e)}>
+        <Icon id={icon} size={size === "small" ? 16 : 32}></Icon>
+        <StyleResetInput
+          placeholder={placeholder}
+          tabIndex="-1"
+          onFocus={(e) => triggerParentFocus(e)}
+        ></StyleResetInput>
+      </StyledInputWrapper>
+    </StyledLabel>
+  );
 };
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const StyledSelect = styled.select`
+  width: min-content;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 8px;
+  border-radius: 4px;
+  border: transparent;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  padding-right: 32px;
+  &::-ms-expand {
+    display: none;
+  }
+
+  &:focus {
+    outline: solid 1px ${COLORS.primary};
+    outline-offset: -1px;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: relative;
+  top: -25px;
+  left: 120px;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
-
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <IconWrapper>
+        <Icon id="chevron-down" size={16} />
+      </IconWrapper>
+    </>
   );
 };
 
